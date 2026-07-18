c73a4d4ccf0f34f498bddee202d68b7ee8aac02a
odiljonovumid90-jpg <odiljonovumid90@gmail.com>
2026-07-18 21:04:29 +0500

    Update smart_bot.py

--- smart_bot.py
+++ smart_bot.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 from flask import Flask
+import threading
 
 TOKEN = '8811211432:AAGsWrVfiOI-Q9EQ4xRCRQdR4sxRh5iB42U'
 bot = telebot.TeleBot(TOKEN)
@@ -19,10 +20,7 @@ def get_phone(message):
     bot.reply_to(message, f"Siz '{message.text}' ni qidirdingiz.")
 
 if __name__ == '__main__':
-    # Flask serverini port 10000 da ishga tushiramiz
     port = int(os.environ.get('PORT', 10000))
-    # Botni polling rejimida ishga tushiramiz
-    import threading
     threading.Thread(target=bot.polling).start()
     app.run(host='0.0.0.0', port=port)
     
