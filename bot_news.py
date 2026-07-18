2bbf562fc1910cf62df08f6e54e8de3481d99de7
odiljonovumid90-jpg <odiljonovumid90@gmail.com>
2026-07-18 17:14:41 +0500

    Update bot_news.py

--- bot_news.py
+++ bot_news.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
+
+
 import telebot
 from gnews import GNews
 
